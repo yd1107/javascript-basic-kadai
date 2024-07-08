a9593bd54b8724676da080b4e53c9c87a7dab927
yd1107 <tomoko1107.ky413@gmail.com>
2024-07-08 14:46:02 +0900

    kadai_mdn_js提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+console.log(today);
+
+const year = today.getFullYear();
+const month = today.getMonth() +1 ;
+const day = today.getDate();
+
+console.log(year + '年' +month +'月' + day + '日');
