7452cd8645827177d724f8e6eb09982bf55715a9
yd1107 <tomoko1107.ky413@gmail.com>
2024-07-13 00:04:39 +0900

    kadai_021提出

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+
+  btn.addEventListener('click', () => {
+    setTimeout( ()=> {
+      console.log('ボタンをクリックしました');
+      text.textContent = 'ボタンをクリックしました';
+    
+    },2000); 
+   
+  } );
+
+
